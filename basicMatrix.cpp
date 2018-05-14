5c8daea2d8d9f4bc2bfd820b2c30ce6eff1eef5e
Cristian Herrera <cdherreram@unal.edu.co>
2018-05-14 16:06:16 +0000

    Introduccion a matrices

---
+++ basicMatrix.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <eigen3/Eigen/Dense>
+
+int main ()
+{
+  Eigen::MatrixXd n(2,2);
+  n = Eigen::MatrixXd::Random(2,2);
+
+  Eigen::MatrixXd m(2,2);
+  m(0,0) = 3;
+  m(1,0) = 2.5;
+  m(0,1) = -1;
+  m(1,1) = m(1,0) + m(0,1);
+
+  std::cout << n << std::endl;
+  std::cout << m << std::endl;
+
+  return 0;
+}
